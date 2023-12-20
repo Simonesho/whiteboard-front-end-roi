730b722c0bc19c94992fa78ad459d8f238056c8c
Simone <144659113+Simonesho@users.noreply.github.com>
2023-12-20 18:22:05 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,40 @@
+    return data
+        print(f"Random Number: {item}")
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+if __name__ == "__main__":
+
+    for item in data:
+    data = generate_random_data()
+    main()
+def generate_random_data():
+    main()
+
+
+        print(f"Random Number: {item}")
+    return data
+def main():
+
+def generate_random_data():
+    data = generate_random_data()
+if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+import random
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
+
+    main()
+
+def main():
+        print(f"Random Number: {item}")
+    for item in data:
+
+    data = generate_random_data()
